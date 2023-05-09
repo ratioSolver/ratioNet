aac75a65af1aaaf22e72f621e7d226a33bb5ab4c
Riccardo De Benedictis <riccardo.debenedictis@istc.cnr.it>
2023-05-09 09:58:19 +0200

    major refactoring..

--- tests/test_client.cpp
+++ tests/test_client.cpp
@@ -1,8 +1,4 @@
-#include "client.h"
-
 int main()
 {
-    network::client client;
-    auto res = client.call(network::request(network::method::GET, "/"));
     return 0;
 }
--- tests/test_server.cpp
+++ tests/test_server.cpp
@@ -1,38 +1,4 @@
-#include "server.h"
-
 int main()
 {
-    network::server server;
-    server.add_route(network::GET, std::regex("/"), [](network::request &req) -> network::response_ptr
-                     { 
-                        std::string page = R"(
-                        <!DOCTYPE html>
-                        <html lang="en">
-                        <head>
-                            <title>Test</title>
-                        </head>
-                        <body>
-                            <h1>Test</h1>
-                            <p>This is a test.</p>
-                        </body>
-
-                        <script type="text/javascript">
-                            var ws = new WebSocket("ws://localhost:8080");
-                            ws.onopen = function() {
-                                ws.send("Hello, world!");
-                            };
-                            ws.onmessage = function (evt) {
-                                var received_msg = evt.data;
-                                alert("Message is received...");
-                            };
-                            ws.onclose = function() {
-                                alert("Connection is closed...");
-                            };
-                        </script>
-
-                        </html>
-                        )";
-                        return new network::html_response(page); });
-    server.start();
     return 0;
 }
