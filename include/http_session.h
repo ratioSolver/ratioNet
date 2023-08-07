307d6915133ac9062878faeca8eb3e58b8b55d06
Riccardo De Benedictis <riccardo.debenedictis@istc.cnr.it>
2023-08-07 16:40:28 +0200

    minor cleanings..

--- include/http_session.h
+++ include/http_session.h
@@ -18,7 +18,21 @@ namespace network
 
     Derived &derived() { return static_cast<Derived &>(*this); }
 
-    void run() {}
+    void do_read()
+    {
+      // Make the request empty before reading,
+      // otherwise the operation behavior is undefined.
+      parser.emplace();
+
+      boost::beast::get_lowest_layer(derived().get_stream()).expires_after(std::chrono::seconds(30));
+
+      // Set the timeout.
+      boost::beast::get_lowest_layer(derived().get_stream()).expires_after(std::chrono::seconds(30));
+
+      // Read a request
+      boost::beast::http::async_read(derived().get_stream(), buffer, *parser, [this](boost::system::error_code ec, size_t bytes_transferred)
+                                     { on_read(ec, bytes_transferred); });
+    }
 
   private:
     void on_read(boost::system::error_code ec, size_t bytes_transferred) {}
@@ -40,6 +54,10 @@ namespace network
   public:
     plain_http_session(boost::beast::tcp_stream &&stream, boost::beast::flat_buffer &&buffer);
 
+    void run();
+
+    boost::beast::tcp_stream &get_stream() { return stream; }
+
   private:
     boost::beast::tcp_stream stream;
   };
@@ -52,6 +70,13 @@ namespace network
   {
   public:
     ssl_http_session(boost::beast::tcp_stream &&stream, boost::asio::ssl::context &ctx, boost::beast::flat_buffer &&buffer);
+
+    void run();
+
+    boost::beast::ssl_stream<boost::beast::tcp_stream> &get_stream() { return stream; }
+
+  private:
+    void on_handshake(boost::system::error_code ec, size_t bytes_used);
 
   private:
     boost::beast::ssl_stream<boost::beast::tcp_stream> stream;
--- src/http_session.cpp
+++ src/http_session.cpp
@@ -1,8 +1,36 @@
 #include "http_session.h"
+#include "logging.h"
 
 namespace network
 {
     plain_http_session::plain_http_session(boost::beast::tcp_stream &&stream, boost::beast::flat_buffer &&buffer) : http_session(std::move(buffer)), stream(std::move(stream)) {}
 
+    void plain_http_session::run() { do_read(); }
+
     ssl_http_session::ssl_http_session(boost::beast::tcp_stream &&stream, boost::asio::ssl::context &ctx, boost::beast::flat_buffer &&buffer) : http_session(std::move(buffer)), stream(std::move(stream), ctx) {}
+
+    void ssl_http_session::run()
+    {
+        // Set the timeout.
+        boost::beast::get_lowest_layer(stream).expires_after(std::chrono::seconds(30));
+
+        // Perform the SSL handshake
+        stream.async_handshake(boost::asio::ssl::stream_base::server, buffer.data(), [this](boost::system::error_code ec, size_t bytes_used)
+                               { on_handshake(ec, bytes_used); });
+    }
+
+    void ssl_http_session::on_handshake(boost::system::error_code ec, size_t bytes_used)
+    {
+        if (ec)
+        {
+            LOG_ERR("SSL handshake failed: " << ec.message());
+            delete this;
+            return;
+        }
+
+        // Consume the portion of the buffer used by the handshake
+        buffer.consume(bytes_used);
+
+        do_read();
+    }
 } // namespace network
