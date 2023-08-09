3804f93d273b90b3ecfe20a5386e5fbe173195a2
Riccardo De Benedictis <riccardo.debenedictis@istc.cnr.it>
2023-08-09 10:53:26 +0200

    minor cleanings..

--- include/server.h
+++ include/server.h
@@ -1,12 +1,367 @@
 #pragma once
 
-#include "http_session.h"
-#include <thread>
-#include <boost/asio.hpp>
+#include "memory.h"
+#include "logging.h"
+#include <boost/beast/core.hpp>
+#include <boost/beast/http.hpp>
 #include <boost/beast/ssl.hpp>
+#include <boost/beast/websocket.hpp>
+#include <boost/asio.hpp>
+#include <queue>
+#include <thread>
+#include <functional>
 
 namespace network
 {
+  class server;
+
+  template <class Derived>
+  class websocket_server_session;
+
+  template <class Derived>
+  class ws_server_handlers
+  {
+    friend class websocket_server_session<Derived>;
+
+  public:
+    ws_server_handlers &on_open(std::function<void(websocket_server_session<Derived> &)> handler) noexcept
+    {
+      on_open_handler = handler;
+      return *this;
+    }
+    ws_server_handlers &on_close(std::function<void(websocket_server_session<Derived> &)> handler) noexcept
+    {
+      on_close_handler = handler;
+      return *this;
+    }
+    ws_server_handlers &on_message(std::function<void(websocket_server_session<Derived> &, const std::string &)> handler) noexcept
+    {
+      on_message_handler = handler;
+      return *this;
+    }
+    ws_server_handlers &on_error(std::function<void(websocket_server_session<Derived> &, boost::system::error_code)> handler) noexcept
+    {
+      on_error_handler = handler;
+      return *this;
+    }
+
+  private:
+    std::function<void(websocket_server_session<Derived> &)> on_open_handler = [](websocket_server_session<Derived> &) {};
+    std::function<void(websocket_server_session<Derived> &)> on_close_handler = [](websocket_server_session<Derived> &) {};
+    std::function<void(websocket_server_session<Derived> &, const std::string &)> on_message_handler = [](websocket_server_session<Derived> &, const std::string &) {};
+    std::function<void(websocket_server_session<Derived> &, boost::system::error_code)> on_error_handler = [](websocket_server_session<Derived> &, boost::system::error_code) {};
+  };
+
+  template <class Derived>
+  class websocket_server_session
+  {
+    Derived &derived() { return static_cast<Derived &>(*this); }
+
+  public:
+    void run(boost::beast::http::request<boost::beast::http::string_body> req)
+    {
+      // Set suggested timeout settings for the websocket
+      derived().get_stream().set_option(boost::beast::websocket::stream_base::timeout::suggested(boost::beast::role_type::server));
+
+      // Set a decorator to change the Server of the handshake
+      derived().get_stream().set_option(boost::beast::websocket::stream_base::decorator([](boost::beast::websocket::response_type &res)
+                                                                                        { res.set(boost::beast::http::field::server, std::string(BOOST_BEAST_VERSION_STRING) + " websocket-server-async"); }));
+
+      // Accept the websocket handshake
+      derived().get_stream().async_accept(req, [this](boost::system::error_code ec)
+                                          { on_accept(ec); });
+    }
+
+  private:
+    void on_accept(boost::system::error_code ec)
+    {
+      if (ec)
+      {
+        LOG_ERR("websocket accept failed: " << ec.message());
+        return;
+      }
+
+      // read a message..
+      do_read();
+    }
+
+    void do_read()
+    {
+      // read a message into our buffer..
+      derived().get_stream().async_read(buffer, [this](boost::system::error_code ec, size_t bytes_transferred)
+                                        { on_read(ec, bytes_transferred); });
+    }
+
+    void on_read(boost::system::error_code ec, size_t)
+    {
+      if (ec == boost::beast::websocket::error::closed)
+        return; // the websocket session was closed..
+
+      if (ec)
+      {
+        LOG_ERR("websocket read failed: " << ec.message());
+        return;
+      }
+    }
+
+    void on_write(boost::system::error_code ec, size_t)
+    {
+      if (ec)
+      {
+        LOG_ERR("websocket write failed: " << ec.message());
+        return;
+      }
+
+      // clear the buffer..
+      buffer.consume(buffer.size());
+
+      // read another message..
+      do_read();
+    }
+
+  protected:
+    boost::beast::flat_buffer buffer;
+  };
+
+  class plain_websocket_server_session : public websocket_server_session<plain_websocket_server_session>
+  {
+  public:
+    plain_websocket_server_session(boost::beast::tcp_stream &&stream) : ws(std::move(stream)) {}
+
+    boost::beast::websocket::stream<boost::beast::tcp_stream> &get_stream() { return ws; }
+
+  private:
+    boost::beast::websocket::stream<boost::beast::tcp_stream> ws;
+  };
+
+  class ssl_websocket_server_session : public websocket_server_session<ssl_websocket_server_session>
+  {
+  public:
+    ssl_websocket_server_session(boost::beast::ssl_stream<boost::beast::tcp_stream> &&stream) : ws(std::move(stream)) {}
+
+    boost::beast::websocket::stream<boost::beast::ssl_stream<boost::beast::tcp_stream>> &get_stream() { return ws; }
+
+  private:
+    boost::beast::websocket::stream<boost::beast::ssl_stream<boost::beast::tcp_stream>> ws;
+  };
+
+  template <class Body, class Allocator>
+  void make_websocket_server_session(boost::beast::tcp_stream stream, boost::beast::http::request<Body, boost::beast::http::basic_fields<Allocator>> req) { (new plain_websocket_server_session(std::move(stream)))->run(std::move(req)); }
+
+  template <class Body, class Allocator>
+  void make_websocket_server_session(boost::beast::ssl_stream<boost::beast::tcp_stream> stream, boost::beast::http::request<Body, boost::beast::http::basic_fields<Allocator>> req) { (new ssl_websocket_server_session(std::move(stream)))->run(std::move(req)); }
+
+  template <class Derived>
+  class http_server_session;
+
+  template <class Derived>
+  struct work
+  {
+  public:
+    work(http_server_session<Derived> &session) : session(session) {}
+    virtual ~work() = default;
+
+    virtual void operator()() = 0;
+
+  protected:
+    void on_write(boost::system::error_code ec, size_t bytes_transferred, bool close) { session.on_write(ec, bytes_transferred, close); }
+
+  protected:
+    http_server_session<Derived> &session;
+  };
+
+  template <class Derived>
+  using work_ptr = utils::u_ptr<work<Derived>>;
+
+  template <class Derived, class Body, class Fields>
+  class server_response : public work<Derived>
+  {
+  public:
+    server_response(http_server_session<Derived> &session, boost::beast::http::message<false, Body, Fields> &&msg) : work<Derived>(session), msg(std::move(msg)) {}
+
+    void operator()() override
+    {
+      bool keep_alive = msg.keep_alive();
+      // Write the response
+      boost::beast::http::async_write(this->session.derived().get_stream(), msg, [this, keep_alive](boost::system::error_code ec, size_t bytes_transferred)
+                                      { this->on_write(ec, bytes_transferred, keep_alive); });
+    }
+
+  private:
+    boost::beast::http::message<false, Body> msg;
+  };
+
+  /**
+   * @brief Base class for HTTP sessions.
+   *
+   */
+  template <class Derived>
+  class http_server_session
+  {
+    friend class work<Derived>;
+
+  public:
+    http_server_session(boost::beast::flat_buffer buffer) : buffer(std::move(buffer)) {}
+
+    Derived &derived() { return static_cast<Derived &>(*this); }
+
+    /**
+     * @brief Read a request from the client.
+     *
+     */
+    void do_read()
+    {
+      parser.emplace();
+
+      // we apply a reasonable limit to the allowed size of the body in bytes to prevent abuse..
+      parser->body_limit(1024 * 1024);
+
+      // we set a timeout..
+      boost::beast::get_lowest_layer(derived().get_stream()).expires_after(std::chrono::seconds(30));
+
+      // we read a request using the parser-oriented interface..
+      boost::beast::http::async_read(derived().get_stream(), buffer, *parser, [this](boost::system::error_code ec, size_t bytes_transferred)
+                                     { on_read(ec, bytes_transferred); });
+    }
+
+    /**
+     * @brief Write a response to the client.
+     *
+     * @return true if the caller should initiate a read operation, false otherwise.
+     */
+    bool do_write()
+    {
+      bool const was_full = response_queue.size() == queue_limit;
+
+      if (!response_queue.empty())
+      { // we send the response..
+        work_ptr<Derived> res = std::move(response_queue.front());
+        response_queue.pop();
+        res->operator()();
+      }
+
+      return was_full;
+    }
+
+  private:
+    void on_read(boost::system::error_code ec, size_t)
+    {
+      if (ec == boost::beast::http::error::end_of_stream)
+      {
+        derived().close();
+        return;
+      }
+      else if (ec)
+      {
+        LOG_ERR("HTTP read failed: " << ec.message());
+        delete this;
+        return;
+      }
+
+      // we see if it is a WebSocket Upgrade..
+      if (boost::beast::websocket::is_upgrade(parser->get()))
+      {
+        // we disable the timeout..
+        boost::beast::get_lowest_layer(derived().get_stream()).expires_never();
+
+        // we transfer the stream to a new WebSocket session..
+        make_websocket_server_session(derived().release_stream(), parser->release());
+        delete this;
+        return;
+      }
+
+      auto req = parser->release();
+
+      // the request path must be absolute and not contain "..".
+      if (req.target().empty() || req.target()[0] != '/' || req.target().find("..") != boost::beast::string_view::npos)
+      {
+        boost::beast::http::response<boost::beast::http::string_body> res{boost::beast::http::status::bad_request, req.version()};
+        res.set(boost::beast::http::field::server, "ratioNet server");
+        res.set(boost::beast::http::field::content_type, "text/html");
+        res.keep_alive(req.keep_alive());
+        res.body() = "Illegal request-target";
+        res.prepare_payload();
+        response_queue.push(new server_response(*this, std::move(res)));
+      }
+
+      if (response_queue.size() == 1)
+        do_write(); // we start the write loop..
+
+      if (response_queue.size() < queue_limit)
+        do_read(); // we pipeline another request..
+    }
+    void on_write(boost::system::error_code ec, size_t, bool keep_alive)
+    {
+      if (ec)
+      {
+        LOG_ERR("HTTP write failed: " << ec.message());
+        delete this;
+        return;
+      }
+
+      if (!keep_alive)
+      { // this means we should close the connection, usually because the response indicated the "Connection: close" semantic..
+        derived().close();
+        return;
+      }
+
+      // we inform the queue that a write completed..
+      if (do_write())
+        do_read();
+    }
+
+  private:
+    static constexpr std::size_t queue_limit = 8; // max responses
+    std::queue<work_ptr<Derived>> response_queue;
+
+    boost::optional<boost::beast::http::request_parser<boost::beast::http::string_body>> parser;
+
+  protected:
+    boost::beast::flat_buffer buffer;
+  };
+
+  /**
+   * @brief HTTP session for a WebSocket connection.
+   *
+   */
+  class plain_http_server_session : public http_server_session<plain_http_server_session>
+  {
+  public:
+    plain_http_server_session(boost::beast::tcp_stream &&stream, boost::beast::flat_buffer &&buffer);
+
+    void run();
+    void close();
+
+    boost::beast::tcp_stream &get_stream() { return stream; }
+    boost::beast::tcp_stream release_stream() { return std::move(stream); }
+
+  private:
+    boost::beast::tcp_stream stream;
+  };
+
+  /**
+   * @brief HTTP session for a WebSocket connection.
+   *
+   */
+  class ssl_http_server_session : public http_server_session<ssl_http_server_session>
+  {
+  public:
+    ssl_http_server_session(boost::beast::tcp_stream &&stream, boost::asio::ssl::context &ctx, boost::beast::flat_buffer &&buffer);
+
+    void run();
+    void close();
+
+    boost::beast::ssl_stream<boost::beast::tcp_stream> &get_stream() { return stream; }
+    boost::beast::ssl_stream<boost::beast::tcp_stream> release_stream() { return std::move(stream); }
+
+  private:
+    void on_handshake(boost::system::error_code ec, size_t bytes_used);
+    void on_shutdown(boost::system::error_code ec);
+
+  private:
+    boost::beast::ssl_stream<boost::beast::tcp_stream> stream;
+  };
+
   /**
    * @brief Detects the session type.
    */
--- src/server.cpp
+++ src/server.cpp
@@ -1,8 +1,66 @@
 #include "server.h"
-#include "logging.h"
 
 namespace network
 {
+    plain_http_server_session::plain_http_server_session(boost::beast::tcp_stream &&stream, boost::beast::flat_buffer &&buffer) : http_server_session(std::move(buffer)), stream(std::move(stream)) {}
+
+    void plain_http_server_session::run() { do_read(); }
+    void plain_http_server_session::close()
+    {
+        stream.socket().shutdown(boost::asio::ip::tcp::socket::shutdown_send);
+        delete this;
+    }
+
+    ssl_http_server_session::ssl_http_server_session(boost::beast::tcp_stream &&stream, boost::asio::ssl::context &ctx, boost::beast::flat_buffer &&buffer) : http_server_session(std::move(buffer)), stream(std::move(stream), ctx) {}
+
+    void ssl_http_server_session::run()
+    {
+        // Set the timeout.
+        boost::beast::get_lowest_layer(stream).expires_after(std::chrono::seconds(30));
+
+        // Perform the SSL handshake
+        stream.async_handshake(boost::asio::ssl::stream_base::server, buffer.data(), [this](boost::system::error_code ec, size_t bytes_used)
+                               { on_handshake(ec, bytes_used); });
+    }
+
+    void ssl_http_server_session::close()
+    {
+        // Set the timeout.
+        boost::beast::get_lowest_layer(stream).expires_after(std::chrono::seconds(30));
+
+        // Perform the SSL shutdown
+        stream.async_shutdown([this](boost::system::error_code ec)
+                              { on_shutdown(ec); });
+    }
+
+    void ssl_http_server_session::on_handshake(boost::system::error_code ec, size_t bytes_used)
+    {
+        if (ec)
+        {
+            LOG_ERR("SSL handshake failed: " << ec.message());
+            delete this;
+            return;
+        }
+
+        // Consume the portion of the buffer used by the handshake
+        buffer.consume(bytes_used);
+
+        do_read();
+    }
+
+    void ssl_http_server_session::on_shutdown(boost::system::error_code ec)
+    {
+        if (ec)
+        {
+            LOG_ERR("SSL shutdown failed: " << ec.message());
+            delete this;
+            return;
+        }
+
+        // At this point the connection is closed gracefully
+        delete this;
+    }
+
     session_detector::session_detector(boost::asio::ip::tcp::socket &&socket, boost::asio::ssl::context &ctx) : stream(std::move(socket)), ctx(ctx) {}
 
     void session_detector::run()
@@ -28,12 +86,12 @@ namespace network
         if (result)
         {
             LOG_DEBUG("Detected SSL session");
-            (new ssl_http_session(std::move(stream), ctx, std::move(buffer)))->run();
+            (new ssl_http_server_session(std::move(stream), ctx, std::move(buffer)))->run();
         }
         else
         {
             LOG_DEBUG("Detected HTTP session");
-            (new plain_http_session(std::move(stream), std::move(buffer)))->run();
+            (new plain_http_server_session(std::move(stream), std::move(buffer)))->run();
         }
     }
 
