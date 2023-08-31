66e765e8b839f061b1e27e6ea36ace0c7d87e049
Riccardo De Benedictis <riccardo.debenedictis@istc.cnr.it>
2023-08-31 16:05:40 +0200

    minor cleanings..
    still to test..

--- include/client.h
+++ include/client.h
@@ -14,11 +14,21 @@ namespace network
     Derived &derived() { return static_cast<Derived &>(*this); }
 
   public:
-    client(boost::asio::strand<boost::asio::system_executor> strand, const std::string &host, const std::string &service) : resolver(strand)
+    client(boost::asio::strand<boost::asio::system_executor> strand, const std::string &host, const std::string &service) : strand(strand), signals(strand), resolver(strand)
     {
+      signals.add(SIGINT);
+      signals.add(SIGTERM);
+#if defined(SIGQUIT)
+      signals.add(SIGQUIT);
+#endif
+      signals.async_wait([this](boost::system::error_code, int)
+                         { close(); });
+
       resolver.async_resolve(host, service, [this](boost::beast::error_code ec, boost::asio::ip::tcp::resolver::results_type results)
                              { on_resolve(ec, results); });
     }
+
+    virtual void close() = 0;
 
   private:
     void on_resolve(boost::beast::error_code ec, boost::asio::ip::tcp::resolver::results_type results)
@@ -43,6 +53,7 @@ namespace network
     boost::asio::strand<boost::asio::system_executor> strand;
 
   private:
+    boost::asio::signal_set signals;
     boost::asio::ip::tcp::resolver resolver;
   };
 
@@ -63,6 +74,11 @@ namespace network
         LOG_ERR("on_connect: " << ec.message());
         return;
       }
+    }
+
+    void close() override
+    {
+      stream.socket().shutdown(boost::asio::ip::tcp::socket::shutdown_both);
     }
 
   private:
@@ -109,6 +125,31 @@ namespace network
       if (ec)
       {
         LOG_ERR("on_handshake: " << ec.message());
+        return;
+      }
+    }
+
+    void close() override
+    {
+      // Set a timeout on the operation
+      boost::beast::get_lowest_layer(stream).expires_after(std::chrono::seconds(30));
+
+      // Perform the SSL shutdown
+      stream.async_shutdown([this](boost::beast::error_code ec)
+                            { on_shutdown(ec); });
+    }
+
+    void on_shutdown(boost::beast::error_code ec)
+    {
+      if (ec == boost::asio::error::eof)
+      {
+        // Rationale:
+        // http://stackoverflow.com/questions/25587403/boost-asio-ssl-async-shutdown-always-finishes-with-an-error
+        ec = {};
+      }
+      if (ec)
+      {
+        LOG_ERR("on_shutdown: " << ec.message());
         return;
       }
     }
