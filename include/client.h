c0325ba3d4847ebccdec6d6b297ae9fb9cb1b706
Riccardo De Benedictis <riccardo.debenedictis@istc.cnr.it>
2023-08-31 17:19:27 +0200

    minor cleanings..
    still to test..

--- include/client.h
+++ include/client.h
@@ -33,6 +33,17 @@ namespace network
     void set_on_error_handler(std::function<void(boost::beast::error_code)> handler) { on_connect_handler = handler; }
     void set_on_close_handler(std::function<void()> handler) { on_close_handler = handler; }
 
+    template <class ReqBody, class ReqFields, class ResBody, class ResFields>
+    void send(boost::beast::http::request<ReqBody, ReqFields> &&req, std::function<void(boost::beast::http::response<ResBody, ResFields> &&, boost::beast::error_code)> handler)
+    {
+      // Set a timeout on the operation
+      boost::beast::get_lowest_layer(derived().get_stream()).expires_after(std::chrono::seconds(30));
+
+      // Send the HTTP request to the remote host
+      boost::beast::http::async_write(derived().get_stream(), req, [this, handler = std::move(handler)](boost::beast::error_code ec, std::size_t bytes_transferred)
+                                      { on_write(handler, ec, bytes_transferred); });
+    }
+
     virtual void close() = 0;
 
   private:
@@ -54,6 +65,37 @@ namespace network
 
     virtual void on_connect(boost::beast::error_code ec, boost::asio::ip::tcp::resolver::results_type::endpoint_type) = 0;
 
+    template <class ResBody, class ResFields>
+    void on_write(std::function<void(boost::beast::http::response<ResBody, ResFields> &&, boost::beast::error_code)> handler, boost::beast::error_code ec, std::size_t)
+    {
+      if (ec)
+      {
+        LOG_ERR("on_write: " << ec.message());
+        on_error_handler(ec);
+        return;
+      }
+
+      auto res = new boost::beast::http::response<ResBody, ResFields>();
+
+      // Receive the HTTP response
+      boost::beast::http::async_read(derived().get_stream(), buffer, *res, [this, handler = std::move(handler), res](boost::beast::error_code ec, std::size_t bytes_transferred)
+                                     { on_read(handler, res, ec, bytes_transferred); });
+    }
+
+    template <class ResBody, class ResFields>
+    void on_read(std::function<void(boost::beast::http::response<ResBody, ResFields> &&, boost::beast::error_code)> handler, boost::beast::http::response<ResBody, ResFields> *res, boost::beast::error_code ec, std::size_t)
+    {
+      if (ec)
+      {
+        LOG_ERR("on_read: " << ec.message());
+        on_error_handler(ec);
+        return;
+      }
+
+      handler(*res, ec);
+      delete res;
+    }
+
   protected:
     boost::asio::strand<boost::asio::system_executor> strand;
     std::function<void()> on_connect_handler = []() {};
@@ -63,6 +105,7 @@ namespace network
   private:
     boost::asio::signal_set signals;
     boost::asio::ip::tcp::resolver resolver;
+    boost::beast::flat_buffer buffer;
   };
 
   class plain_client : public client<plain_client>
