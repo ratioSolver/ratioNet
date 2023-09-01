ec1be8fa3aa30d0a96572998b080361d3d783839
Riccardo De Benedictis <riccardo.debenedictis@istc.cnr.it>
2023-09-01 15:25:13 +0200

    added certificates..

--- include/server.h
+++ include/server.h
@@ -721,11 +721,10 @@ namespace network
         thread.join();
     }
 
-    void set_ssl_context(const std::string &certificate_chain_file, const std::string &private_key_file, const std::string &dh_file)
+    void set_ssl_context(const std::string &certificate_file, const std::string &private_key_file)
     {
-      ctx.use_certificate_chain_file(certificate_chain_file);
+      ctx.use_certificate_file(certificate_file, boost::asio::ssl::context::pem);
       ctx.use_private_key_file(private_key_file, boost::asio::ssl::context::pem);
-      ctx.use_tmp_dh_file(dh_file);
     }
 
   private:
@@ -754,7 +753,7 @@ namespace network
     std::vector<std::thread> threads;                                 // The thread pool
     boost::asio::signal_set signals;                                  // The signal_set is used to register for process termination notifications
     boost::asio::ip::tcp::endpoint endpoint;                          // The endpoint for the server
-    boost::asio::ssl::context ctx{boost::asio::ssl::context::tlsv12}; // The SSL context is required, and holds certificates
+    boost::asio::ssl::context ctx{boost::asio::ssl::context::sslv23}; // The SSL context is required, and holds certificates
     boost::asio::ip::tcp::acceptor acceptor;                          // The acceptor receives incoming connections
     std::unordered_map<boost::beast::http::verb, std::vector<std::pair<std::regex, utils::u_ptr<http_handler>>>> http_routes, https_routes;
     std::vector<std::pair<std::regex, utils::u_ptr<ws_handler>>> ws_routes, wss_routes;
--- tests/test_server.cpp
+++ tests/test_server.cpp
@@ -19,9 +19,33 @@ void test_simple()
   delete t;
 }
 
+void test_ssl()
+{
+  network::server server;
+
+  server.set_ssl_context("mycert.pem", "mykey.pem");
+
+  server.add_route(boost::beast::http::verb::get, "/", std::function{[](const boost::beast::http::request<boost::beast::http::string_body> &, boost::beast::http::response<boost::beast::http::string_body> &res)
+                                                                     {
+                                                                       res.set(boost::beast::http::field::content_type, "html");
+                                                                       res.body() = R"(<html><body><h1>Hello, world!</h1></body></html>)";
+                                                                     }},
+                   true);
+
+  auto t = new std::thread{[&server]
+                           { server.start(); }};
+
+  std::this_thread::sleep_for(std::chrono::seconds(10));
+  server.stop();
+  t->join();
+  delete t;
+}
+
 int main()
 {
   test_simple();
+
+  test_ssl();
 
   return 0;
 }
