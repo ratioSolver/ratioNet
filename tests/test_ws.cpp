85c8726719b062e5af1ebc3a4d55dcaf7081cd21
Riccardo De Benedictis <riccardo.debenedictis@cnr.it>
2023-10-30 15:33:17 +0100

    improved documentation..

--- tests/test_ws.cpp
+++ tests/test_ws.cpp
@@ -4,7 +4,13 @@
 
 void test_ws()
 {
-    network::plain_ws_client client;
+    network::plain_ws_client client(
+        "echo.websocket.org", "80", "/",
+        [&client]()
+        { std::cout << "Connected!" << std::endl;
+          client.send("Hello, world!"); },
+        [](const std::string &msg)
+        { std::cout << "Message from server: " << msg << std::endl; });
     std::this_thread::sleep_for(std::chrono::seconds(5));
     client.send("Hello, world!");
     std::this_thread::sleep_for(std::chrono::seconds(5));
@@ -12,7 +18,13 @@ void test_ws()
 
 void test_wss()
 {
-    network::ssl_ws_client client;
+    network::ssl_ws_client client(
+        "echo.websocket.org", "443", "/",
+        [&client]()
+        { std::cout << "Connected!" << std::endl;
+          client.send("Hello, world!"); },
+        [](const std::string &msg)
+        { std::cout << "Message from server: " << msg << std::endl; });
     std::this_thread::sleep_for(std::chrono::seconds(5));
     client.send("Hello, world!");
     std::this_thread::sleep_for(std::chrono::seconds(5));
