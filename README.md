9e7222b42793bbebee1c6f4639a2ae1abc5e9d25
Riccardo De Benedictis <riccardo.debenedictis@cnr.it>
2023-10-30 11:36:47 +0100

    removed utils dependency..

--- README.md
+++ README.md
@@ -22,8 +22,6 @@ Follow these steps to get started with `ratioNet`:
 Clone the Repository: Start by cloning this repository to your local machine:
 
 ```bash
-sh
-Copy code
 git clone https://github.com/ratioSolver/ratioNet.git
 ```
 
@@ -34,10 +32,20 @@ Here's a simple example of how to use `ratioNet` to create an HTTP server:
 ```cpp
 #include <server.h>
 
+using string_req = boost::beast::http::request<boost::beast::http::string_body>;
+using string_res = boost::beast::http::response<boost::beast::http::string_body>;
+
 int main()
 {
     // Create the server instance
     network::server server;
+
+    // Add a GET route to the server
+    server.add_route(boost::beast::http::verb::get, "/", std::function{[](const string_req &, string_res &res)
+    {
+        res.set(boost::beast::http::field::content_type, "html");
+        res.body() = R"(<html><body><h1>Hello, world!</h1></body></html>)";
+    }});
 
     // Start the server
     server.start();
--- include/server.h
+++ include/server.h
@@ -1,6 +1,5 @@
 #pragma once
 
-#include "logging.h"
 #include <boost/asio.hpp>
 #include <boost/asio/ssl.hpp>
 #include <boost/beast.hpp>
@@ -11,6 +10,40 @@
 #include <regex>
 #include <thread>
 #include <queue>
+
+#ifdef VERBOSE_LOG
+#include <iostream>
+
+#ifdef WIN32
+#define COLOR_NORMAL ""
+#define COLOR_RED ""
+#define COLOR_GREEN ""
+#define COLOR_YELLOW ""
+#else
+#define COLOR_NORMAL "\033[0m"
+#define COLOR_RED "\033[31m"
+#define COLOR_GREEN "\033[32m"
+#define COLOR_YELLOW "\033[33m"
+#endif
+
+#define LOG_ERR(msg) std::cerr << COLOR_RED << __FILE__ << "(" << __LINE__ << "): " << msg << COLOR_NORMAL << std::endl
+#define LOG_WARN(msg) std::clog << COLOR_YELLOW << __FILE__ << "(" << __LINE__ << "): " << msg << COLOR_NORMAL << std::endl
+#define LOG_DEBUG(msg) std::clog << COLOR_GREEN << __FILE__ << "(" << __LINE__ << "): " << msg << COLOR_NORMAL << std::endl
+#define LOG(msg) std::cout << COLOR_NORMAL << __FILE__ << "(" << __LINE__ << "): " << msg << COLOR_NORMAL << std::endl
+#else
+#define LOG_ERR(msg) \
+  {                  \
+  }
+#define LOG_WARN(msg) \
+  {                   \
+  }
+#define LOG_DEBUG(msg) \
+  {                    \
+  }
+#define LOG(msg) \
+  {              \
+  }
+#endif
 
 namespace network
 {
