1822238d43b4cb4050d94d0cb411ee9127c5bfe2
Riccardo De Benedictis <riccardo.debenedictis@cnr.it>
2024-02-08 10:54:52 +0100

    Add GET macro for adding routes to the server

--- tests/test_server.cpp
+++ tests/test_server.cpp
@@ -5,6 +5,8 @@
 using string_req = boost::beast::http::request<boost::beast::http::string_body>;
 using string_res = boost::beast::http::response<boost::beast::http::string_body>;
 
+#define GET(server, target, handler) server.add_route(boost::beast::http::verb::get, target, std::function{handler})
+
 void test_plain_async_server()
 {
     network::async::server server;
@@ -12,11 +14,10 @@ void test_plain_async_server()
                            { std::cout << msg << std::endl; });
     server.set_error_handler([](const std::string &msg)
                              { std::cerr << msg << std::endl; });
-    server.add_route(boost::beast::http::verb::get, "/", std::function{[](const string_req &, string_res &res)
-                                                                       {
-                                                                           res.set(boost::beast::http::field::content_type, "html");
-                                                                           res.body() = R"(<html><body><h1>Hello, world!</h1></body></html>)";
-                                                                       }});
+    GET(server, "/", [](const string_req &, string_res &res)
+        {
+            res.set(boost::beast::http::field::content_type, "html");
+            res.body() = R"(<html><body><h1>Hello, world!</h1></body></html>)"; });
 
     std::thread t{[&server]
                   { server.start(); }};
