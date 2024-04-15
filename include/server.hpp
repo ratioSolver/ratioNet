1ebef996e11fdd8950094a47a17e1caae3b82cde
Riccardo De Benedictis <riccardo.debenedictis@cnr.it>
2024-04-15 15:42:37 +0200

    Refactor session class in server.hpp and server.cpp to handle HTTP verbs

--- include/server.hpp
+++ include/server.hpp
@@ -12,10 +12,12 @@ namespace network
     DELETE
   };
 
+  class server;
+
   class session : public std::enable_shared_from_this<session>
   {
   public:
-    session(boost::asio::ip::tcp::socket socket);
+    session(server &srv, boost::asio::ip::tcp::socket socket);
     ~session();
 
     void start();
@@ -24,12 +26,33 @@ namespace network
     void on_read(const boost::system::error_code &ec, std::size_t bytes_transferred);
 
   private:
+    server &srv;
     boost::asio::ip::tcp::socket socket;
     boost::asio::streambuf buffer;
   };
 
+  class request
+  {
+  public:
+    request(std::shared_ptr<session> s, verb v, std::string &&trgt, std::string &&ver, std::map<std::string, std::string> &&hdrs) : s(s), v(v), target(trgt), version(ver), headers(hdrs) {}
+
+    verb get_verb() const { return v; }
+    const std::string &get_target() const { return target; }
+    const std::string &get_version() const { return version; }
+    const std::map<std::string, std::string> &get_headers() const { return headers; }
+
+  private:
+    std::shared_ptr<session> s;
+    verb v;
+    std::string target;
+    std::string version;
+    std::map<std::string, std::string> headers;
+  };
+
   class server
   {
+    friend class session;
+
   public:
     server(const std::string &address = "0.0.0.0", unsigned short port = 8080, std::size_t concurrency_hint = std::thread::hardware_concurrency());
 
@@ -41,6 +64,8 @@ namespace network
   private:
     void do_accept();
     void on_accept(const boost::system::error_code &ec, boost::asio::ip::tcp::socket socket);
+
+    void handle_request(request &&req);
 
   private:
     boost::asio::io_context io_ctx;          // The io_context is required for all I/O
--- src/server.cpp
+++ src/server.cpp
@@ -3,10 +3,10 @@
 
 namespace network
 {
-    session::session(boost::asio::ip::tcp::socket socket) : socket(std::move(socket)) {}
+    session::session(server &srv, boost::asio::ip::tcp::socket socket) : srv(srv), socket(std::move(socket)) {}
     session::~session() {}
 
-    void session::start() { boost::asio::async_read_until(socket, buffer, '\r\n\r\n', std::bind(&session::on_read, shared_from_this(), std::placeholders::_1, std::placeholders::_2)); }
+    void session::start() { boost::asio::async_read_until(socket, buffer, "\r\n\r\n", std::bind(&session::on_read, shared_from_this(), std::placeholders::_1, std::placeholders::_2)); }
 
     void session::on_read(const boost::system::error_code &ec, std::size_t bytes_transferred)
     {
@@ -22,35 +22,58 @@ namespace network
         switch (is.get())
         {
         case 'D':
-            if (is.get() == 'E' && is.get() == 'L' && is.get() == 'E' && is.get() == 'T' && is.get() == 'E' && is.get() == ' ')
+            if (is.get() == 'E' && is.get() == 'L' && is.get() == 'E' && is.get() == 'T' && is.get() == 'E')
                 v = DELETE;
             break;
         case 'G':
-            if (is.get() == 'E' && is.get() == 'T' && is.get() == ' ')
+            if (is.get() == 'E' && is.get() == 'T')
                 v = GET;
             break;
         case 'P':
             switch (is.get())
             {
             case 'O':
-                if (is.get() == 'S' && is.get() == 'T' && is.get() == ' ')
+                if (is.get() == 'S' && is.get() == 'T')
                     v = POST;
                 break;
             case 'U':
-                if (is.get() == 'T' && is.get() == ' ')
+                if (is.get() == 'T')
                     v = PUT;
                 break;
             }
             break;
         }
+        is.get(); // consume space
 
         std::string target;
-        while (is.get() != ' ')
+        while (is.peek() != ' ')
             target += is.get();
+        is.get(); // consume space
 
         std::string version;
-        while (is.get() != '\n')
+        while (is.peek() != '\r')
             version += is.get();
+        is.get(); // consume '\r'
+        is.get(); // consume '\n'
+
+        std::map<std::string, std::string> headers;
+        while (is.peek() != '\r')
+        {
+            std::string header, value;
+            while (is.peek() != ':')
+                header += is.get();
+            is.get(); // consume ':'
+            is.get(); // consume space
+            while (is.peek() != '\r')
+                value += is.get();
+            is.get(); // consume '\r'
+            is.get(); // consume '\n'
+            headers.emplace(std::move(header), std::move(value));
+        }
+        is.get(); // consume '\r'
+        is.get(); // consume '\n'
+
+        srv.handle_request(request(shared_from_this(), v, std::move(target), std::move(version), std::move(headers)));
     }
 
     server::server(const std::string &address, unsigned short port, std::size_t concurrency_hint) : io_ctx(concurrency_hint), endpoint(boost::asio::ip::make_address(address), port), acceptor(boost::asio::make_strand(io_ctx)) { threads.reserve(concurrency_hint); }
@@ -99,8 +122,30 @@ namespace network
     void server::on_accept(const boost::system::error_code &ec, boost::asio::ip::tcp::socket socket)
     {
         if (!ec)
-            std::make_shared<session>(std::move(socket))->start();
+            std::make_shared<session>(*this, std::move(socket))->start();
 
         do_accept();
+    }
+
+    void server::handle_request(request &&req)
+    {
+        switch (req.get_verb())
+        {
+        case GET:
+            std::cout << "GET ";
+            break;
+        case POST:
+            std::cout << "POST ";
+            break;
+        case PUT:
+            std::cout << "PUT ";
+            break;
+        case DELETE:
+            std::cout << "DELETE ";
+            break;
+        }
+        std::cout << req.get_target() << " " << req.get_version() << std::endl;
+        for (const auto &[header, value] : req.get_headers())
+            std::cout << header << ": " << value << std::endl;
     }
 } // namespace network
