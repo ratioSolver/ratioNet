50a05d09f48d51709bc1bb7e49f8641beb19d883
Riccardo De Benedictis <riccardo.debenedictis@cnr.it>
2024-04-17 18:19:18 +0200

    Add sleep to main function in test_server.cpp

--- tests/test_server.cpp
+++ tests/test_server.cpp
@@ -1,9 +1,12 @@
+#include <thread>
 #include "server.hpp"
 
 int main(int argc, char const *argv[])
 {
     network::server server;
     server.start();
+
+    std::this_thread::sleep_for(std::chrono::seconds(10));
 
     return 0;
 }
